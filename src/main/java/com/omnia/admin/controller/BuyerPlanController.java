4e37265f9c95c4f9594256ec5f5f00ae9cc7cd3e
bkorinnyi <bohdan.korinnyi@gmail.com>
2017-11-18 15:07:12 +0200

    buyer plan path

--- src/main/java/com/omnia/admin/controller/BuyerPlanController.java
+++ src/main/java/com/omnia/admin/controller/BuyerPlanController.java
@@ -2,14 +2,16 @@ package com.omnia.admin.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("buyer")
 public class BuyerPlanController {
-    @GetMapping
+    @GetMapping("plan")
     public ResponseEntity getPlan(@RequestParam List<String> month, @RequestParam List<String> buyers) {
 
         return ResponseEntity.ok().build();
