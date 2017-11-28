5992999064277838c61378c24b86538ae69dba79
AntonMisiura <misiura.anton@gmail.com>
2017-11-29 01:44:32 +0200

    new page cost management

--- src/main/resources/static/scripts/controllers/costManagementController.js
+++ src/main/resources/static/scripts/controllers/costManagementController.js
@@ -31,10 +31,6 @@ Application.controller("costManagementController", function ($scope, $http, date
 
     $scope.tableSpent = "";
 
-    $scope.selectedPage = 1;
-    $scope.totalPagination = 1;
-    $scope.noOfPages = 1;
-
     //functions
     $scope.getRole = function () {
         var request = new XMLHttpRequest();
@@ -45,54 +41,6 @@ Application.controller("costManagementController", function ($scope, $http, date
             var z = JSON.parse(request.response);
             $scope.role = z.authorities[0].authority;
         }
-    };
-
-
-    $scope.loadCosts = function () {
-        $scope.costs = [];
-        $scope.showCostManagementLoader = true;
-        $scope.getRole();
-        if($scope.role === "BUYER"){
-            $scope.hideBuyerSelect = true;
-        }
-
-        var url = "/expenses?buyerIds="+ $scope.getFilterDetails();
-
-        $http.post(url, $scope.getSizeAndNumberFilter())
-            .then(function successCallback(response) {
-                $scope.costs = response.data;
-                $scope.showCostManagementLoader = false;
-                $scope.totalPagination = response.data.size;
-                $scope.noOfPages = Math.ceil($scope.totalPagination / $scope.selectedSize);
-            }, function errorCallback(response) {
-                $scope.showCostManagementLoader = false;
-                notify('ti-alert', 'Error occurred during loading postbacks', 'danger');
-            });
-    };
-
-
-    $scope.getFilterDetails = function () {
-        var fromDate = "";
-        var toDate = "";
-        if ($scope.selectedDate !== 'no-date') {
-            if ($scope.selectedDate === 'custom') {
-                fromDate = $scope.dpFromDate;
-                toDate = $scope.dpToDate;
-            }
-            else {
-                fromDate = formatDate(dateFactory.pickDateFrom($scope.selectedDate));
-                toDate = formatDate(dateFactory.pickDateTo($scope.selectedDate));
-            }
-        }
-
-        return $scope.selectedBuyerNames+"&expensesType="+$scope.selectedTypes+"&from="+fromDate+"&to="+toDate;
-    };
-
-    $scope.getSizeAndNumberFilter = function () {
-        var parameters = {};
-        parameters.page = $scope.selectedPage;
-        parameters.size = $scope.selectedSize;
-        return parameters;
     };
 
 
@@ -107,9 +55,14 @@ Application.controller("costManagementController", function ($scope, $http, date
 
 
     $scope.getTypes = function () {
-        var url = "expenses/type/all";
+        var url = "/account/types";
         $http.get(url).then(function success(response) {
-            $scope.types = response.data;
+            for (var i = 0; i < response.data.length; i++) {
+                $scope.types.push({
+                    id: i,
+                    name: response.data[i]
+                });
+            }
         }, function fail(response) {
             notify('ti-alert', 'Error occurred during loading types', 'danger');
         });
