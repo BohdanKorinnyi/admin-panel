9aeb8dbe44b6c848500ac9505d7e1c2af27890f4
bkorinnyi <bohdan.korinnyi@gmail.com>
2017-12-01 00:45:32 +0200

    revert all

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -1,13 +1,3 @@
-var menuFlag = true;
-$('#minimizeSidebar').on('click', function () {
-    if (menuFlag) {
-        $('body').addClass("sidebar-mini");
-    } else {
-        $('body').removeClass("sidebar-mini");
-    }
-    menuFlag = !menuFlag;
-});
-
 var Application = angular.module("Application", [
     "ngRoute",
     "ngResource",
@@ -21,7 +11,7 @@ Application.constant("roles", {
     BUYER: 1,
     TEAM_LEADER: 2,
     CBO: 3,//Media Buyer Director
-    MENTOR: 4,
+    MENTOR:4,
     CFO: 5,//Chief Finance Office
     DIRECTOR: 6,
     ADMIN: 7
