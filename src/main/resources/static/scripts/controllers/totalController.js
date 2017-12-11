21146b3599d3453789f3865422dcd10972b389f7
bkorinnyi <bohdan.korinnyi@gmail.com>
2017-12-12 01:30:01 +0200

    fix

--- src/main/resources/static/scripts/controllers/totalController.js
+++ src/main/resources/static/scripts/controllers/totalController.js
@@ -1,45 +1,32 @@
 Application.controller('totalController', function ($scope, $http) {
+    $scope.selectedMonth = 'December';
+    $scope.selectedSize = 50;
     $scope.sizeOptions = {
         50: 50,
         100: 100,
         500: 500
     };
 
-    $scope.selectedSize = 50;
-
-    $scope.dateOptions = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-    ];
-    $scope.selectedMonth = [];
-
-    $scope.buyersTotals = {};
+    $scope.dateOptions = {
+        'January': 'January',
+        'February': 'February',
+        'March': 'March',
+        'April': 'April',
+        'May': 'May',
+        'June': 'June',
+        'July': 'July',
+        'August': 'August',
+        'September': 'September',
+        'October': 'October',
+        'November': 'November',
+        'December': 'December'
+    };
     $scope.data = [];
 
     $scope.init = function () {
-        var url = "/finance/total?from=2017-10-01&to=2017-10-31";
+        var url = "/finance/total?from=2017-09-01&to=2017-12-31";
         $http.get(url).then(function success(response) {
-            $scope.buyersTotals = response.data;
-            Object.keys($scope.buyersTotals).map(function (p1) {
-                $scope.data.push($scope.buyersTotals[p1]);
-            });
-            console.log($scope.data);
-            for(var i = 0; i<2; i++){
-                console.log($scope.data[i]);
-                for(var j=0; j<2; j++){
-                    console.log($scope.data[i][j]);
-                }
-            }
+            $scope.data = response.data;
         }, function error() {
             notify('ti-alert', 'Error occurred during loading data', 'danger');
         });
