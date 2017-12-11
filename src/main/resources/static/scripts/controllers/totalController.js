5ec3b602ec284610ba93a68348ff2e09bb04e92c
AntonMisiura <misiura.anton@gmail.com>
2017-12-12 00:11:58 +0200

    req

--- src/main/resources/static/scripts/controllers/totalController.js
+++ src/main/resources/static/scripts/controllers/totalController.js
@@ -1,4 +1,4 @@
-Application.controller('totalController', function ($scope) {
+Application.controller('totalController', function ($scope, $http) {
     $scope.sizeOptions = {
         50: 50,
         100: 100,
@@ -22,4 +22,15 @@ Application.controller('totalController', function ($scope) {
         'December'
     ];
     $scope.selectedMonth = [];
+
+    $scope.data = [];
+    
+    $scope.init = function () {
+        var url = "/finance/total?from=2017-09-01&to=2017-12-31";
+        $http.get(url).then(function success(response) {
+            $scope.data = response.data;
+        }, function error() {
+            notify('ti-alert', 'Error occurred during loading data', 'danger');
+        });
+    };
 });
