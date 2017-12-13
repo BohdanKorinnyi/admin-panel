16e13119309dc063f3288a1645087d9b683fadbf
AntonMisiura <misiura.anton@gmail.com>
2017-12-14 00:25:49 +0200

    all fixes and chart date this/last month fix

--- src/main/resources/static/scripts/controllers/adminDashboardController.js
+++ src/main/resources/static/scripts/controllers/adminDashboardController.js
@@ -203,13 +203,14 @@ function getDateOfWeek(w, y) {
 
 function formatDateWithoutYear(date) {
     var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
+        //month = '' + (d.getMonth() + 1),
         day = '' + d.getDate();
 
-    if (month.length < 2) month = '0' + month;
+    //if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
-    return [month, day].join('-');
+    //return [month, day].join('-');
+    return [day];
 }
 
 
