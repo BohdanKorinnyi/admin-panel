0b89b4770417679f8459b2e03af25bde09f2a74b
AntonMisiura <misiura.anton@gmail.com>
2017-12-27 14:17:58 +0200

    fix this year filter

--- src/main/resources/static/scripts/controllers/advertiserBalanceController.js
+++ src/main/resources/static/scripts/controllers/advertiserBalanceController.js
@@ -29,14 +29,27 @@ Application.controller("advertiserBalanceController", function ($scope, $http, d
         var dateFrom = "";
         var dateTo = "";
         if ($scope.selectedInterval !== 'custom') {
-            dateFrom = formatDate(dateFactory.pickDateFrom($scope.selectedInterval));
-            dateTo = formatDate(dateFactory.pickDateTo($scope.selectedInterval));
+            if ($scope.selectedInterval === 'allTime'){
+                var yearFrom = new Date().getFullYear();
+                var yearTo = new Date().getFullYear() + 1;
+
+                var f = new Date(yearFrom, 0, 1);
+                var t = new Date(yearTo, 0, 1);
+
+                dateFrom = formatDate(f);
+                dateTo = formatDate(t);
+            }
+            else {
+                dateFrom = formatDate(dateFactory.pickDateFrom($scope.selectedInterval));
+                dateTo = formatDate(dateFactory.pickDateTo($scope.selectedInterval));
+            }
         }
         else {
             dateFrom = formatDate($scope.dpFromDate);
             dateTo = formatDate($scope.dpToDate);
         }
         var url = '/advertiser/report?advertiserIds=' + ($scope.selectedAdv.join()) + '&from=' + dateFrom + '&to=' + dateTo;
+        console.log(url);
         $http.get(url).then(function successCallback(response) {
             $scope.totalRevenue = response.data.totalRevenue;
             $scope.totalIncome = response.data.totalIncome;
