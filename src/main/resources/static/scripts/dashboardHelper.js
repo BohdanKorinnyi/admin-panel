4612177d798e8e915033ed8e7afa294758789e69
bkorinnyi <bohdan.korinnyi@gmail.com>
2018-01-26 13:01:09 +0200

    added helper

---
+++ src/main/resources/static/scripts/dashboardHelper.js
@@ -0,0 +1,45 @@
+function tableRow(month, values) {
+    var revenue = getDataByMonth(month, values[0].data);
+    var spent = getDataByMonth(month, values[1].data);
+    var paid = getDataArrayByMonth(month, values[2].data);
+    var bonus = getDataByMonth(month, values[3].data);
+    return {
+        month: month,
+        revenue: revenue,
+        spent: spent,
+        bonus: bonus,
+        paid: paid,
+        paidTotal: calculatePaid(paid),
+        profit: (revenue - spent).toFixed(2)
+    };
+}
+
+function calculatePaid(payments) {
+    var result = 0;
+    payments.map(function (value) {
+
+    });
+    return result;
+}
+
+function getDataByMonth(month, data) {
+    var monthValue = 0;
+    data.map(function (value) {
+        if (value.date === month) {
+            monthValue = value.value;
+        }
+    });
+    return monthValue;
+}
+
+function getDataArrayByMonth(month, data) {
+    var monthValue = [];
+    if (Array.isArray(data)) {
+        data.map(function (value) {
+            if (value.date === month) {
+                monthValue.push(value.value);
+            }
+        });
+    }
+    return monthValue;
+}
