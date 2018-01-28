4b5d2ee466b56dbc2eca61019bd5a0256d40f292
AntonMisiura <misiura.anton@gmail.com>
2018-01-28 13:20:54 +0200

    format view date

--- src/main/resources/static/scripts/controllers/dashboardController.js
+++ src/main/resources/static/scripts/controllers/dashboardController.js
@@ -120,6 +120,18 @@ Application.controller('dashboardController', function ($scope, $http, $q) {
             notify('ti-alert', 'Error occurred during loading dashboard chart info', 'danger');
         });
     };
+
+    $scope.formatViewDate = function (date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [day, month, year].join('-');
+    };
 });
 
 
