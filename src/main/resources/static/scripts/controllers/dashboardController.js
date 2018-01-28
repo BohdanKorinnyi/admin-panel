c32151f9c1bf874e0cbfadce42baa34f7283fa88
AntonMisiura <misiura.anton@gmail.com>
2018-01-28 13:30:15 +0200

    upd date

--- src/main/resources/static/scripts/controllers/dashboardController.js
+++ src/main/resources/static/scripts/controllers/dashboardController.js
@@ -122,15 +122,7 @@ Application.controller('dashboardController', function ($scope, $http, $q) {
     };
 
     $scope.formatViewDate = function (date) {
-        var d = new Date(date),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear();
-
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-
-        return [day, month, year].join('-');
+        return formatViewDate(date);
     };
 });
 
